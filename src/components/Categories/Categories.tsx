74e322314fa67fe4eec57e416f56508d6d84c020
ex1one <vyvf11@gmail.com>
2022-06-18 17:09:03 +0500

    Made a sorting component

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Categories = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [, set] = useState();
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Categories;
